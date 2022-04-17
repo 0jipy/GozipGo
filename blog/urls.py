4c46c69da511f8dcf81d742710f7f485bbd9c5c0
0jipy <96100083+0jipy@users.noreply.github.com>
2022-04-17 17:53:40 +0900

    "포스트 상세 페이지에 부트스트랩 적용하기"

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,8 @@ from django.urls import URLPattern, path
 from . import views
 
 urlpatterns = [
-    path('', views.PostList.as_view()),
-    # path('', views.index),    
-    path('<int:pk>/', views.single_post_page),
+    path('<int:pk>/', views.PostDetail.as_view()),
+    path('', views.PostList.as_view()),       
+    # path('<int:pk>/', views.single_post_page),
+    # path('', views.index), 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,15 @@
 from re import template
-from django.shortcuts import render
-from django.views.generic import ListView
+# from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from requests import post
 from .models import Post
 
 class PostList(ListView):
     model = Post
     ordering = '-pk'
+
+class PostDetail(DetailView):
+    model = Post
 
 # def index(request):
 #     posts = Post.objects.all().order_by('-pk')
@@ -18,13 +22,14 @@ class PostList(ListView):
 #         }
 #     )
 
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
+# 
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk)
 
-    return render(
-        request, 
-        'blog/single_post_page.html',
-        {
-            'post': post,
-        }
-    )
+#     return render(
+#         request, 
+#         'blog/single_post_page.html',
+#         {
+#             'post': post,
+#         }
+#     )
